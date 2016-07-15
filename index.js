e7e92fc4ad0ea0311a3602a63de1a5560d64a547
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2016-07-15 19:34:09 +0530

    add table rows

--- index.js
+++ index.js
@@ -1,23 +1,152 @@
-var Component = React.createClass({
-  propTypes: {
-    name: React.PropTypes.string.isRequired,
-  },
-  render: function() {
-    return React.DOM.h1(null, "I am "+ this.props.name)
+"use strict";
+
+var Td = React.createClass({
+  displayName: "Td",
+
+  render: function render() {
+    return React.createElement(
+      "tr",
+      null,
+      React.createElement(
+        "td",
+        null,
+        React.createElement("input", { type: "checkbox", onClick: this.props.doSum })
+      ),
+      this.props.tdData.map(function (cell) {
+        return React.createElement(
+          "td",
+          null,
+          cell
+        );
+      })
+    );
   }
 });
 
-ReactDOM.render(
-  React.DOM.h1({
-    id: "my_header",
-    className: "my-header"
-  }, "Hello Bugu"),
-  document.getElementById("app")
-);
+var Excel = React.createClass({
+  displayName: "Excel",
 
-ReactDOM.render(
-  React.createElement(Component, {
-    name: "Bugu"
-  }),
-  document.getElementById("app")
-);
+  getInitialState: function getInitialState() {
+    return {
+      sum: 0
+    };
+  },
+  propTypes: {
+    header: React.PropTypes.arrayOf(React.PropTypes.string),
+    data: React.PropTypes.arrayOf(React.PropTypes.string)
+  },
+
+  _sum: function _sum(a, b) {
+    return a + b;
+  },
+
+  _selectAll: function _selectAll(data) {
+    var _this = this;
+
+    return function (ev) {
+      var sum = 0;
+      var checkBoxes = document.getElementsByTagName("input");
+
+      if (ev.target.checked) {
+        for (var i = 0; i < checkBoxes.length; i++) {
+          checkBoxes[i].checked = true;
+        }
+
+        data.map(function (i) {
+          return Number(i[3]);
+        }).forEach(function (i) {
+          return sum = sum + i;
+        });
+        _this.setState({ sum: sum });
+      } else {
+        for (var i = 0; i < checkBoxes.length; i++) {
+          checkBoxes[i].checked = false;
+        }
+
+        _this.setState({ sum: 0 });
+      }
+    };
+  },
+
+  _handle: function _handle(price) {
+    var _this2 = this;
+
+    return function (ev) {
+      if (!ev.target.checked) {
+        price = price * -1;
+      }
+
+      _this2.setState({
+        sum: _this2.state.sum + Number(price)
+      });
+    };
+  },
+
+  render: function render() {
+    var _this3 = this;
+
+    var els = function els(data) {
+      return data.map(function (cell) {
+        return React.createElement(
+          "td",
+          null,
+          cell
+        );
+      });
+    };
+
+    return React.createElement(
+      "table",
+      { id: "table" },
+      React.createElement(
+        "thead",
+        null,
+        React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "td",
+            null,
+            React.createElement("input", { type: "checkbox", onClick: this._selectAll(this.props.data) })
+          ),
+          this.props.header.map(function (title, i) {
+            return React.createElement(
+              "td",
+              { key: i },
+              title
+            );
+          })
+        )
+      ),
+      React.createElement(
+        "tbody",
+        null,
+        this.props.data.map(function (datum, idx) {
+          return React.createElement(Td, { key: idx, tdData: datum, doSum: _this3._handle(datum[3]) });
+        }),
+        React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "td",
+            { colSpan: "4" },
+            "Total"
+          ),
+          React.createElement(
+            "td",
+            { className: "total" },
+            this.state.sum.toFixed(2)
+          )
+        )
+      )
+    );
+  }
+});
+
+var tableHeader = ["Book", "Author", "Language", "Price"];
+var tableData = [["Ruby", "Matsumoto", "English", "110.8"], ["React", "Zukerberg", "Hibru", "100.5"]];
+
+ReactDOM.render(React.createElement(Excel, {
+  header: tableHeader,
+  data: tableData
+}), document.getElementById("app"));
--- table.js
+++ table.js
@@ -1,4 +1,21 @@
+var Td = React.createClass({
+  render: function() {
+    return <tr>
+      <td>
+        <input type="checkbox" onClick={this.props.doSum}/>
+      </td>
+      {this.props.tdData.map(function(cell) { return <td>{cell}</td> })}
+    </tr>
+  }
+});
+
+
 var Excel = React.createClass({
+  getInitialState: function() {
+    return {
+      sum: 0
+    }
+  },
   propTypes: {
     header: React.PropTypes.arrayOf(
       React.PropTypes.string
@@ -7,35 +24,78 @@ var Excel = React.createClass({
       React.PropTypes.string
     )
   },
+
+  _sum: function(a, b) {
+    return a + b;
+  },
+
+  _selectAll: function(data) {
+    return (ev) => {
+      var sum = 0;
+      var checkBoxes = document.getElementsByTagName("input");
+
+      if(ev.target.checked) {
+        for(var i=0; i< checkBoxes.length; i++) {
+          checkBoxes[i].checked = true;
+        }
+
+        data.map(i => Number(i[3])).forEach(function(i) { return sum = sum + i })
+        this.setState({ sum: sum});
+      }
+      else {
+        for(var i=0; i< checkBoxes.length; i++) {
+          checkBoxes[i].checked = false;
+        }
+
+        this.setState({ sum: 0});
+      }
+    }
+  },
+
+  _handle: function(price) {
+    return (ev) => {
+      if(!ev.target.checked) {
+        price = price * -1
+      }
+
+      this.setState({
+        sum: this.state.sum + Number(price)
+      })
+    }
+  },
+
   render: function() {
-    return React.DOM.table(
-      { className: "table" },
-      React.DOM.thead(null,
-                      React.DOM.tr(null,
-                                   this.props.header.map(function(title, idx) {
-                                     return React.DOM.th({key: idx}, title);
-                                   })
-                                  )),
-      React.DOM.tbody(null,
-                      this.props.data.map(function(datum, idx) {
-                        return React.DOM.tr({key: idx},
-                                            datum.map(function(cell, idx){
-                                              return React.DOM.td({key: idx}, cell);
-                                            }))
-                      }))
-
+    return (
+      <table id="table">
+        <thead>
+          <tr>
+        <td><input type="checkbox" onClick={this._selectAll(this.props.data)} /></td>
+            {this.props.header.map((title, i) => <td key={i}>{title}</td> )}
+          </tr>
+        </thead>
+        <tbody>
+        { this.props.data.map((datum, idx) => {
+          return <Td key={idx} tdData={datum} doSum={this._handle(datum[3])} />;
+        })
+        }
+          <tr>
+            <td colSpan="4">Total</td>
+            <td className="total">{this.state.sum.toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
     )
-
   }
 });
 
-var tableHeader = ["Book", "Author", "Language"];
-var tableData = [["Ruby", "Matsumoto", "English"], ["React", "Zukerberg", "Hibru"]];
+
+var tableHeader = ["Book", "Author", "Language", "Price"];
+var tableData = [["Ruby", "Matsumoto", "English", "110.8"], ["React", "Zukerberg", "Hibru", "100.5"]];
 
 ReactDOM.render(
   React.createElement(Excel, {
     header: tableHeader,
-    data: tableData
+    data: tableData,
   }),
   document.getElementById("app")
 );
